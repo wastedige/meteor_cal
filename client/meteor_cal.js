3d4273d63053958249b7cc01460108e0dd8b66f9
Shaahin <shaahin@gmail.com>
2015-06-13 16:54:45 -0500

    Adds to collection and renders correctly elsewhere but doesn't add to collection here.

--- client/meteor_cal.js
+++ client/meteor_cal.js
@@ -1,38 +1,64 @@
 //Session only works on the client
 
-CalEvents = new Mongo.Collection("calevents");
+CalEvents = new Mongo.Collection("z3");
 // to be used later to handle editing
-Session.setDefault("event2edit", null);
-Session.setDefault("showEditWindow", false);
-
 
 
 if (Meteor.isClient) {
+    Session.setDefault("event2edit", null);
+    Session.setDefault("showEditWindow", false);
+    Session.setDefault("lastMod", null);
+    Router.route('/', function () {
+        this.render('home');
+    });
 
+    Router.route('/calendar', function () {
+        this.render('calendar');
+    });
+
+    // runs when page has been rendered
+    Template.calendar.rendered = function () {
+        var cal = $('#calendar').fullCalendar({
+            // http://fullcalendar.io/docs/usage/
+
+            dayClick: function(date, allDay, jsEvent, view){
+                var calendarEvent = {};
+                calendarEvent.start = date;
+                calendarEvent.end = date;
+                calendarEvent.title = "New !";
+                //Meteor.call('saveCalEvent', calendarEvent );
+                CalEvents.insert(calendarEvent);
+                updateCalendar();
+                alert(CalEvents.find().count())
+            },
+            eventClick: function (calEvent, jsEvent, view) {
+
+            },
+            events: function (start, end, callback) {
+                calEvents = CalEvents.find().fetch();
+                callback(calEvents);
+
+            }
+        });
+    }
+
+    Template.calendar.lastMod = function () {
+        return Session.get('lastMod');
+    }
+}
+
+var updateCalendar = function(){
+	$('#calendar').fullCalendar( 'refetchEvents' );
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+
+    Meteor.startup(function () {
+        Meteor.methods({
+            'saveCalEvent': function(ce){
+                CalEvents.insert(ce);
+
+            }
+        })
+    });
 }
-
-// runs when page has been rendered
-Template.calendar.rendered = function(){
-  $('#calendar').fullCalendar({
-    // http://fullcalendar.io/docs/usage/
-    events: [
-         {
-             title  : 'event1',
-             start  : '2015-05-01'
-         }]
-  });
-}
-
-Router.route('/', function () {
-  this.render('home');
-});
-
-Router.route('/calendar', function () {
-  this.render('calendar');
-});
