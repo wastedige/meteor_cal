e494fead06e0dd28b4267d2b16a4da10646b6059
Shaahin Shahbazi <shaahin@gmail.com>
2015-06-17 14:02:49 -0500

    Dialog responds to event click. Deps. Removed manual update.

--- client/meteor_cal.js
+++ client/meteor_cal.js
@@ -4,17 +4,29 @@ CalEvents = new Mongo.Collection("z3");
 // to be used later to handle editing
 
 
+
 if (Meteor.isClient) {
     Meteor.subscribe('calCollection');
-    Session.setDefault("event2edit", null);
-    Session.setDefault("showEditWindow", false);
-    Session.setDefault("lastMod", null);
+
     Router.route('/', function () {
         this.render('home');
     });
 
     Router.route('/calendar', function () {
         this.render('calendar');
+    });
+
+    Session.setDefault('editing_event', null);
+
+    Template.main.helpers({
+      editing_event: function(){
+        return Session.get('editing_event');
+      }
+    });
+    Template.dialog.events({
+      'click .closeDialog': function(event, template){
+        Session.set('editing_event', null);
+      }
     });
 
     // runs when page has been rendered
@@ -27,30 +39,36 @@ if (Meteor.isClient) {
                 calendarEvent.start = date;
                 calendarEvent.end = date;
                 calendarEvent.title = "New !";
+                calendarEvent.owner = Meteor.userId(); //does it work?
                 //Meteor.call('saveCalEvent', calendarEvent );
                 CalEvents.insert(calendarEvent);
-                updateCalendar();
+                //updateCalendar();
                 //alert(CalEvents.find().count())
             },
             eventClick: function (calEvent, jsEvent, view) {
-
+                Session.set('editing_event', calEvent._id);
             },
             events: function (start, end, callback) {
                 calEvents = CalEvents.find().fetch();
                 callback(calEvents);
 
             }
-        });
+        }).data().fullCalendar;
+
+        Deps.autorun(function(){
+          CalEvents.find().fetch();
+          if (cal) {
+            cal.refetchEvents();
+          }
+        })
     }
 
-    Template.calendar.lastMod = function () {
-        return Session.get('lastMod');
-    }
 }
 
-var updateCalendar = function(){
-	$('#calendar').fullCalendar( 'refetchEvents' );
-}
+// Deps already takes care of this:
+//var updateCalendar = function(){
+//	$('#calendar').fullCalendar( 'refetchEvents' );
+//}
 
 if (Meteor.isServer) {
 
